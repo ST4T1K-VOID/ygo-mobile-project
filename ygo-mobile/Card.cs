cb765b2bff769e1a1bc5aaf6d2d86801c4653d32
ST4T1K <mjobrien06@hotmail.com>
2025-04-14 21:03:32 +0800

    feat: add 'Card' constructor

--- ygo-mobile/Card.cs
+++ ygo-mobile/Card.cs
@@ -10,7 +10,7 @@ namespace ygo_mobile
     {
         public string ID { get; set; }
         public string Name { get; set; }
-        public string desc { get; set; }
+        public string Description { get; set; }
 
 
         public string Type { get; set; }
@@ -18,16 +18,33 @@ namespace ygo_mobile
         public string Attribute { get; set; }
 
         public int Level { get; set; }
-        public int Atk { get; set; }
-        public int Def { get; set; }
+        public int? Atk { get; set; }
+        public int? Def { get; set; }
 
         public List<string> CardImages { get; set; }
 
-        //misc info
-        public List<string> CardSets { get; set; }
-        //cardsets this card is in
-        public List<string> CardPrices { get; set; }
+        //--misc info--
+        public List<string>? CardSets { get; set; }
+        //cardsets the card is in
+
+        //public List<string>? CardPrices { get; set; }
         //NOTE: might not keep this
+
+        
+        public Card(string iD, string name, string desc, string type, string race, string attribute, int level, int atk, int def, List<string> cardImages, List<string> cardSets, List<string> cardPrices)
+        {
+            ID = iD;
+            Name = name;
+            Description = desc;
+            Type = type;
+            Race = race;
+            Attribute = attribute;
+            Level = level;
+            Atk = atk;
+            Def = def;
+            CardImages = cardImages;
+            CardSets = cardSets;
+        }
     }
 
 }
