2c1de479167e92a60058dae8b3ca9719b9a76b0a
ST4T1K <mjobrien06@hotmail.com>
2025-04-14 20:48:42 +0800

    feat: add Card data class and properties

---
+++ ygo-mobile/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ygo_mobile
+{
+    public class Card
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string desc { get; set; }
+
+
+        public string Type { get; set; }
+        public string Race { get; set; }
+        public string Attribute { get; set; }
+
+        public int Level { get; set; }
+        public int Atk { get; set; }
+        public int Def { get; set; }
+
+        public List<string> CardImages { get; set; }
+
+        //misc info
+        public List<string> CardSets { get; set; }
+        //cardsets this card is in
+        public List<string> CardPrices { get; set; }
+        //NOTE: might not keep this
+    }
+
+}
+
