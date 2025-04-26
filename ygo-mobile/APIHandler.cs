95039d612fb09ac8eeea6a0ebdd57368ec13e052
ST4T1K <mjobrien06@hotmail.com>
2025-04-26 13:23:01 +0800

    feat: add APIHandler boilerplate

---
+++ ygo-mobile/APIHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ygo_mobile
+{
+    internal class APIHandler
+    {
+
+        public async Task<List<string>> GetArchetypes()
+        {
+            return new List<string>();
+        }
+
+
+        public async Task<List<Card>> RequestCards()
+        {
+            return new List<Card>();
+        }
+
+        public string FormatRequest(List<Dictionary<string, string>> searchVariables)
+        {
+            return string.Empty;
+        }
+    }
+}
