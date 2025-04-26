a4f8f5e4be59191b59557be082c8c4be046d1841
ST4T1K <mjobrien06@hotmail.com>
2025-04-26 13:22:01 +0800

    chore: various cosmetic and semantic tweaks

--- ygo-mobile/card-details.xaml.cs
+++ ygo-mobile/card-details.xaml.cs
@@ -3,7 +3,7 @@ namespace ygo_mobile;
 public partial class card_details : ContentPage
 {
 	//TODO: Remove mock data after testing
-    public Card? CurrentCard = new Card("46986416", "Dark Magician", "'The ultimate wizard in terms of attack and defense''", "Normal monster", "spellcaster", "DARK", 7, 2500, 2100);
+    public Card? CurrentCard = new Card("46986416", "Dark Magician", "'The ultimate wizard in terms of attack and defense'", "Normal monster", "spellcaster", "DARK", 7, 2500, 2100);
 
     public card_details()
 	{
@@ -19,21 +19,22 @@ public partial class card_details : ContentPage
     }
 
 	/// <summary>
-	/// replaces placeholder images with card specefic data
+	/// Fills the page with data from the current card
 	/// </summary>
 	public void UpdatePage()
 	{
 		if (CurrentCard != null)
 		{
-            //TODO: Check for chached image before fetching the url image
-            image_card.Source = CurrentCard.UrlToImage("baseImage").Source;
+			//TODO: Check for chached image before fetching the url image
+			//TODO: try/catch >> Image not found//placeholder image
+			image_card.Source = CurrentCard.UrlToImage("baseImage").Source;
 
 
 			image_attribute.Source = ImageSource.FromFile($"attribute_{CurrentCard.Attribute}.png");
 			label_attribute.Text = CurrentCard.Attribute;
 
-			//cleans tribe value to match image naming conventions (alphanumeric/'_')
-            image_tribe.Source = ImageSource.FromFile($"tribe_{CurrentCard.Tribe.Replace("-","").ToLower()}.png");
+			//cleans tribe value to match image naming conventions (alphanumeric & '_')
+			image_tribe.Source = ImageSource.FromFile($"tribe_{CurrentCard.Tribe.Replace("-", "").ToLower()}.png");
 			label_tribe.Text = CurrentCard.Tribe;
 
 			label_name.Text = CurrentCard.Name;
@@ -42,11 +43,12 @@ public partial class card_details : ContentPage
 			label_atk.Text = $"ATK: {CurrentCard.Atk.ToString()}|";
 			label_def.Text = $"|DEF: {CurrentCard.Def.ToString()}";
 
-			label_description.Text = CurrentCard.Description;
+			label_description.Text = $"{CurrentCard.Description}";
 
 			label_carddata.Text = $"{CurrentCard.Tribe}||{CurrentCard.Type}";
 			
 			return;
         }
+		return;
 	}
 }
