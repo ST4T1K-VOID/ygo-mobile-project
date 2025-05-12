0fba2f3343e5236985d459e38bdfa7492de08959
ST4T1K-VOID <mjobrien06@hotmail.com>
2025-05-12 13:28:32 +0800

    feat: add portrait/landscape layouts

--- ygo-mobile/SearchResults.xaml.cs
+++ ygo-mobile/SearchResults.xaml.cs
@@ -5,5 +5,12 @@ public partial class search_results : ContentPage
 	public search_results()
 	{
 		InitializeComponent();
+		testFunc();
 	}
+
+	public void testFunc()
+	{
+		view_TEST.ItemsSource = new byte[8];
+        view_TEST_grid.ItemsSource = new byte[8];
+    }
 }
