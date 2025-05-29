c0195abbadd3019caa8be7446c9f95a72b7e92ca
ST4T1K-VOID <mjobrien06@hotmail.com>
2025-05-29 12:26:08 +0800

    feat: cont implementation of cardetails display method

--- ygo-mobile/CardDetails.xaml.cs
+++ ygo-mobile/CardDetails.xaml.cs
@@ -18,79 +18,76 @@ public partial class card_details : ContentPage
 	{
 		//------remove------
         APIReqeustHandler aPIReqeustHandler = new APIReqeustHandler();
-		string longname = "Endymion,%20the%20Mighty%20Master%20of%20Magic";
 
-        List<Card> cards = await aPIReqeustHandler.SendRequest(new Dictionary<string, string> { {"name","pot%20of%20greed" } });
-        MonsterCard currentCard = null;
-
-        Card card = cards[0];
+        List<Card> cards = await aPIReqeustHandler.SendRequest(new Dictionary<string, string> { {"type", "Pendulum Effect Monster" } });
+        Card currentCard = cards[0];
         //-------------------
 
-        if (card == null)
+        if (currentCard == null)
         {
             return;
         }
 
         try
         {
-            image_card.Source = card.UrlToImage("image_url").Source;
+            image_card.Source = currentCard.UrlToImage("image_url").Source;
         }
         catch
         {
             //use placeholder image
         }
 
-        label_name.Text = card.Name;
-        label_cardName.Text = card.Name;
-        label_description.Text = card.Description;
+        label_name.Text = currentCard.Name;
+        label_cardName.Text = currentCard.Name;
+        label_description.Text = currentCard.Description;
 
         //cleans tribe value to match image naming conventions (alphanumeric & '_')
-        image_tribe.Source = ImageSource.FromFile($"tribe_{card.Tribe.Replace("-", "").Replace(" ","")}.png");
-        label_tribe.Text = card.Tribe;
-        image_attribute.Source = ImageSource.FromFile($"attribute_{card.Type.Replace(" ", "")}.png");
-        label_attribute.Text = card.Type;
+        image_tribe.Source = ImageSource.FromFile($"tribe_{currentCard.Tribe.Replace("-", "").Replace(" ","")}.png");
+        label_tribe.Text = currentCard.Tribe;
+        image_attribute.Source = ImageSource.FromFile($"attribute_{currentCard.Type.Replace(" ", "")}.png");
+        label_attribute.Text = currentCard.Type;
 
 
-        label_carddata.Text = $"{card.Tribe}||{card.Type}";
+        label_carddata.Text = $"{currentCard.Tribe}||{currentCard.Type}";
 
 
-		if (card is MonsterCard)
+		if (currentCard is MonsterCard)
 		{
-            image_attribute.Source = ImageSource.FromFile($"attribute_{currentCard.Attribute}.png");
-            label_attribute.Text = currentCard.Attribute;
+            MonsterCard ConvertedCard = (MonsterCard)currentCard;
 
-            label_atk.Text = $"ATK: {currentCard.Atk.ToString()}|";
-            label_def.Text = $"|DEF: {currentCard.Def.ToString()}";
+            image_attribute.Source = ImageSource.FromFile($"attribute_{ConvertedCard.Attribute}.png");
+            label_attribute.Text = ConvertedCard.Attribute;
+
+            label_atk.Text = $"ATK: {ConvertedCard.Atk.ToString()}|";
+            label_def.Text = $"|DEF: {ConvertedCard.Def.ToString()}";
             
-            if (currentCard.Type.Contains("XYZ"))
+            if (ConvertedCard.Type.Contains("XYZ"))
             {
-                list_rank.ItemsSource = new byte[(int)currentCard.Level];
+                list_rank.ItemsSource = new byte[(int)ConvertedCard.Level];
                 border_rank.IsVisible = true;
             }
             else
             {
-                list_level.ItemsSource = new byte[(int)currentCard.Level];
+                list_level.ItemsSource = new byte[(int)ConvertedCard.Level];
                 border_level.IsVisible = true;
+            }
+            if (ConvertedCard.Type == "Link Monster")
+            {
+                //display unique properties
             }
             
         }
 		if (currentCard is PendulumMonster)
 		{
+            PendulumMonster ConvertedCard = (PendulumMonster)currentCard;
 
+            label_pend_description.Text = ConvertedCard.PendDesc;
+            border_pendulum_desc.IsVisible = true;
+
+            //display unique properties
 		}
 		
 
-		{
-			//TODO: Check for chached image before fetching the url image
-			//TODO: try/catch >> Image not found//placeholder image
-
-
-
-
-
-
-			return;
-		}
 		return;
 	}
 }
