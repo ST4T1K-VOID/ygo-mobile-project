fed0312336dee80f588c7f184f194f3a55d0ef99
ST4T1K <mjobrien06@hotmail.com>
2025-06-10 18:30:45 +0800

    feat: add ability to reselect an item after leaving card details screen

--- ygo-mobile/SearchResults.xaml.cs
+++ ygo-mobile/SearchResults.xaml.cs
@@ -3,30 +3,34 @@ namespace ygo_mobile;
 public partial class SearchResults : ContentPage
 {
 	List<Card> Cards = new List<Card>();
+    Card SelectedCard = null;
 
 	public SearchResults( List<Card> cards = null)
 	{
 		InitializeComponent();
-        //collection_resulstsPortrait.SelectedItem = null;
-        //collection_resulstsLandscape.SelectedItem = null;
-		testFunc();
-		//if cards == empty display no results found message
-	}
-
-	public async void testFunc()
-	{
-        APIReqeustHandler handler = new APIReqeustHandler();
-		Cards = await handler.SendRequest(new Dictionary<string, string> { { "fname", "toon" } });
+        Cards = cards;
 
         collection_resulstsPortrait.ItemsSource = Cards;
         collection_resulstsLandscape.ItemsSource = Cards;
-	}
+        //if cards == empty display no results found message
+    }
+
+    private async void button_back_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PopModalAsync();
+    }
 
     private async void collection_resulstsPortrait_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
 		try
 		{
-			await Navigation.PushModalAsync(new cardDetails(collection_resulstsPortrait.SelectedItem as Card));
+            SelectedCard = collection_resulstsPortrait.SelectedItem as Card;
+            if (SelectedCard == null)
+            {
+                return;
+            }
+			await Navigation.PushModalAsync(new cardDetails(SelectedCard));
+            collection_resulstsPortrait.SelectedItem = null;
         }
 		catch
 		{
@@ -39,7 +43,13 @@ public partial class SearchResults : ContentPage
     {
         try
         {
+            SelectedCard = collection_resulstsLandscape.SelectedItem as Card;
+            if (SelectedCard == null)
+            {
+                return;
+            }
             await Navigation.PushModalAsync(new cardDetails(collection_resulstsLandscape.SelectedItem as Card));
+            collection_resulstsLandscape.SelectedItem = null;
         }
         catch
         {
